0811fdc600086c50ce76c68c339a4899393f75f7
luisMendoza00 <71712459+luisMendoza00@users.noreply.github.com>
2020-10-26 19:37:40 -0600

    Rectangulo y triangulo

--- Juego.py
+++ Juego.py
@@ -54,15 +54,36 @@ def circle(start, end):
     r = math.sqrt( ((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2) )/2
     t.circle(r) 
     end_fill()
+
 #funcion para dibujar un rectangulo
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward(end.y - start.y)
+        left(90)
+
+    end_fill()
 
 #funcion para dibujar un triangulo
 def triangle(start, end):
     "Draw triangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+
+    end_fill()
 
 #Funcion para guardar las posiciones de los clicks
 def tap(x, y):
